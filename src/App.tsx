7482b5e3b53ec7c9d22c0166516b1169cb3a39fe
Tausif4171 <tausifkhan4171@gmail.com>
2023-08-17 14:05:44 +0530

    full calendar react component setup ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Calendar from './Calendar';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hii</h1>
+      <Calendar />
     </div>
   );
 }
---
+++ src/Calendar.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+function Calendar() {
+  return (
+    <div>
+         <div className='demo-app-main'>
+          <FullCalendar
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            headerToolbar={{
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            }}
+            initialView='dayGridMonth'
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+           
+          />
+        </div>
+    </div>
+  )
+}
+
+export default Calendar
